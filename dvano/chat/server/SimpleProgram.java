b4ebefc45bf5367b4aa68f113ca05dc3a341ec77
dvano <dvano@mail.ua>
2014-07-01 18:21:11 +0300

    Create SimpleProgram.java

---
+++ dvano/chat/server/SimpleProgram.java
@@ -0,0 +1,34 @@
+package dvano.chat.server;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author dvano
+ */
+public final class SimpleProgram {
+
+    private SimpleProgram() {
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.exit(1);
+        }
+
+        try {
+            final Server server = new Server(Integer.valueOf(args[0]));
+            final Thread thread = new Thread(server);
+            thread.setDaemon(true);
+            thread.start();
+            server.update();
+        } catch (IOException ex) {
+            Logger.getLogger(SimpleProgram.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
